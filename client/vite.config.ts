8076b79b8baa2c1a11f44594d2dec5d400a86458
Wenfei Lin <wl688728@gmail.com>
2025-12-13 08:21:48 -0600

    Fix vite.config to run preview on port 5173

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   ],
   server: { // useful when i was using a backend in the beginning
     host: '127.0.0.1', // To allow the website to be accessed by http://127.0.0.1:5173/ on Windows; Linux is less picky
+    port: 5173,
     proxy: {
       "/api": {
         target: "http://localhost:5000",
@@ -18,4 +19,7 @@ export default defineConfig({
       },
     },
   },
+  preview: {
+    port: 5173, // Otherwise, it'll run on 4173 by standard, which my Spotify App does not include in its list of callback paths (it only includes port 5173)
+  }
 });
